7a6a70be98819bafd7f97329dab3203db2c0f600
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-08 13:35:10 +0400

    add filterFalsyValues function

--- task5.js
+++ task5.js
@@ -1,2 +1,16 @@
 // Write a function that implements filtering in array
 'use strict';
+const values = [null, true, {}, { name: 'Elon' }, '', NaN, 0];
+
+function filterFalsyValues() {
+  function falsey() {
+    const falseyValues = values.filter((e) => {
+      if (!e) {
+        e;
+      }
+    });
+  }
+  return falsey;
+}
+let falseyVal = filterFalsyValues(values);
+console.log(falseyVal());
