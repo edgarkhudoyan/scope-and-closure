3d909a671048958d9a3d5d811738e6f76ed39b29
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-08 13:22:18 +0400

    add print function

--- task4.js
+++ task4.js
@@ -1,2 +1,14 @@
 // Create a function printAfter that calls its argument after printing hello world
 'use strict';
+
+function printAfter(name) {
+  console.log('Hello, world');
+
+  const print = () => {
+    console.log(name);
+  };
+
+  print();
+}
+
+printAfter('Elon Musk');
