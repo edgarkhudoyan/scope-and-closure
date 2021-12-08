501c1fa672f4389d5fc942114530689102e0f02c
Edgar Khudoyan <khudoyanedgar@gmail.com>
2021-12-08 12:25:02 +0400

    declare makeAdder function

--- task1.js
+++ task1.js
@@ -1,2 +1,12 @@
 // Create a function that implements partial sum
 'use strict';
+function MakeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+let addOne = MakeAdder(1);
+let addTen = MakeAdder(10);
+
+console.log(addTen(2));
